c4ed4116ea531d7bbe581cc254860c212bb50ba9
unknown <andreaquiroga010@gmail.com>
2025-06-19 15:04:16 -0500

    Practica

---
+++ src/main/java/com/mycompany/practica3/Practica3.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.practica3;
+
+/**
+ *
+ * @author DELL
+ */
+public class Practica3 {
+
+    public static void main(String[] args) {
+        System.out.println("agregar documentos");
+    }
+}
